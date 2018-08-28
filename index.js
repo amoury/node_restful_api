cada6d78149ee29d96c375462be964317ed09cad
Amoury <ansar.memon@hotmail.com>
2018-08-28 12:14:48 +0400

    Check CRU Endpoints created

--- index.js
+++ index.js
@@ -115,6 +115,7 @@ const unifiedServer = (req, res) => {
 const router = {
   'ping': handlers.ping,
   'users': handlers.users,
-  'tokens': handlers.tokens
+  'tokens': handlers.tokens,
+  'checks': handlers.checks
 };
 
